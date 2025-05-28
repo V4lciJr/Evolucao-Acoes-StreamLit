9cbd7b38e378a47e58f72e4971dae0e0c77957e0
Valci Júnior <valcijunior19@hotmail.com>
2025-05-28 08:07:24 -0300

    adicionado performance das acoes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import yfinance as yf
+import pandas as pd
 from datetime import timedelta
 
 
 # função de carregamento de dados
+# carrega as informações de ações de janeiro de 2015 à abril de 2025
 @st.cache_data
 def carregar_dados(empresas):
     lista_acoes = " ".join(empresas)
@@ -12,8 +14,17 @@ def carregar_dados(empresas):
     cotacoes_acao = cotacoes_acao["Close"]
     return cotacoes_acao
 
+# carrega a base contendo os códigos das ações
+@st.cache_data
+def carregar_tickers_acoes():
+    base_tickers = pd.read_csv('IBOV.csv', sep=';')
+    tickers = list(base_tickers['Código'])
+    tickers = [item + ".SA" for item in tickers]
 
-acoes = ["ITUB4.SA", "PETR4.SA", "MGLU3.SA", "VALE3.SA", "ABEV3.SA", "GGBR4.SA"]
+    return tickers
+
+
+acoes = carregar_tickers_acoes()
 dados = carregar_dados(acoes)
 
 st.write("""
@@ -43,5 +54,25 @@ intervalo_data = st.sidebar.slider("Selecione o período desejado",
                                    step=timedelta(days=30))
 
 dados = dados.loc[intervalo_data[0]: intervalo_data[1]]
+
 # grafico de area
 st.line_chart(dados)
+
+texto_performance_ativo = ''
+
+if len(lista_acoes) == 0:
+    lista_acoes = list(dados.columns)
+elif len(lista_acoes) == 1:
+    dados = dados.rename(columns={"Close": acao_unica})
+
+for acao in lista_acoes:
+    performance_ativo = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1
+    performance_ativo = float(performance_ativo)
+    texto_performance_ativo += f'  \n{acao}: {performance_ativo:.1%}'
+
+st.write(f"""
+### Performance dos Ativos
+Abaixo temos a performance de ativo selecionado
+
+{texto_performance_ativo}
+""")
